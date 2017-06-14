fe3b9bfee1136078d3b5174dc586e4d4a4565bf1
caseyanderson <casey.thomas.anderson@gmail.com>
2017-06-14 13:57:42 -0700

    reorg

---
+++ scholar_processor.py
@@ -0,0 +1,11 @@
+'''
+
+usage:
+
+1. pipe output of scholar.py query into a text file
+2. import text file into python, parse to dictionary (view one), then maybe to google doc spreadsheet(view 2)
+3. need to have a way to go back to
+
+'''
+
+f = open()
