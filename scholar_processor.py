61f66d83fefc9ff8ac5f548dfe9ffc01ae4bc0e3
caseyanderson <casey.thomas.anderson@gmail.com>
2017-06-28 14:03:37 -0700

    updates

--- scholar_processor.py
+++ scholar_processor.py
@@ -25,10 +25,7 @@ entries = []
 pairs = []
 prevstart = 0
 
-scholar_fields = ['Title', 'URL', 'Year', 'Citations', 'Cluster ID', 'Citations list', 'Versions list', 'Excerpt']
-
-
-num_new_articles=1 # allows for a running total display of separate articles
+scholar_fields = ['Title', 'URL', 'Year', 'Citations', 'Versions', 'Cluster ID', 'Citations list', 'Versions list', 'Excerpt']
 
 f = open(path, 'r')
 results = f.read()
@@ -51,15 +48,12 @@ for num, info in enumerate(clean):
 ## sets up the list of entry dictionaries
 separated_entries = list( {} for i in entries )
 
-# this will build the dictionaries, per entry, all stored in a list
-for h, i in enumerate(entries):
-    print()
-    print(i)
-    for j, k in enumerate(clean, start=prevstart):
-        if j <= (prevstart + 7):
-            print(k)
+## i seem to have to use range here
+def my_range(start, end, step):
+    while start <= end:
+        print(clean[start])
+        start += step
 
-            continue
-        else:
-            prevstart = i
-            break
+for i in entries:
+    x = i
+    my_range(x, x + 8, 1)
