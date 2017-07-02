ce3ba4417f58eb096364141b383184a782add6aa
caseyanderson <casey.thomas.anderson@gmail.com>
2017-07-02 11:37:12 -0700

    almost done with mostly automatic creation of dictionaries

--- scholar_processor.py
+++ scholar_processor.py
@@ -7,28 +7,29 @@ python3 scholar.py --phrase "digital_humanities" > "$(date +"%Y_%m_%d_%I_%M_%p")
 
 python3 scholar.py --phrase "gramophone, film, typewriter" > "$(date +"%Y_%m_%d_%I_%M_%p").txt"
 
-2. import text file into python, parse to dictionary (view one), then maybe to google doc spreadsheet(view 2)
+2. import text file into python, parse to dictionary (view 1), then maybe to google doc spreadsheet(view 2)
 
 
 TODO:
 
-1. does scholar.py limit search results? check...we only want first 20 per stage
-2. scholar_processor should be able to do the query and handle processing the output simultaneously
-3. processor output should include subject or something that allows me to identify which output came from what search
+1. scholar_processor should be able to do the query and handle processing the output simultaneously
+2. processor output should include subject or something that allows me to identify which output came from what search
 4. do the query first and then have a way to check to see citation block that each result comes from (this seems easiest to look at in a spreadsheet?)
 
 '''
 
 base = '/Users/mdp/git/scholar.py_trina/'
-input_file='2017_06_28_03_57_PM.txt'
+input_file='2017_07_02_10_52_AM.txt'
 path = ''.join([base, input_file])
 clean = []
 start_entries = []
 end_entries = []
 pairs = []
-# prevstart = 0
+sequences=[]
 
-scholar_fields = ['Title', 'URL', 'Year', 'Citations', 'Versions', 'Cluster ID', 'Citations list', 'Versions list', 'Excerpt']
+#scholar_fields = ['Title', 'URL', 'Year', 'Citations', 'Versions', 'Cluster ID', 'Citations list', 'Versions list' 'Excerpt']
+scholar_fields = ['Title', 'URL', 'Year', 'Citations list', 'Excerpt']
+
 
 f = open(path, 'r')
 results = f.read()
@@ -55,7 +56,7 @@ for num, info in enumerate(clean):
 
 
 ## sets up the list of entry dictionaries
-separated_entries = list( {} for i in entries )
+separated_entries = list( {} for i in start_entries )
 
 ## this makes an index list from start of article to end of article (pulling from clean)
 
@@ -63,15 +64,22 @@ def num_sequence(start, end):
     x = list(range(start, end))
     return x
 
-# generates a tuple of start and ends
+# combines start and end list into one tuple
 zipped = list(zip(start_entries, end_entries))
 
 for i,j in zipped:
     sequences.append(list(num_sequence(i, j)))
 
-## this breaks up the sequences so they can be iterated through later, this will build the dictionaries
-for i in sequences:
-    blah = list(i)
-    for j in blah:
-        print(j)
-        sleep(1)
+## iterates through number sequences per entry, splits by article, checks to see which tags are used and formats dictionary properly
+## this works but somehow citation list gets counted twice
+for num, info in enumerate(sequences):
+    the_range = list(info)
+    for j in the_range:
+        for k in scholar_fields:
+            if clean[j].startswith(k):
+                blah = clean[j].split(k)
+                entry = blah[1]
+                separated_entries[num][str(k)]=entry
+
+## having some problem with Citations list info overwriting Citations somehow so just adding Citations info on a second pass
+## this happens with Versions and Versions list too...not sure why
